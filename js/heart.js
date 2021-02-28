b339178c246a9d27b37d22a0864dffb34fa23d78
Lily Meng <LilyxMeng@github.com>
2021-02-28 11:05:17 -0500

    commit

--- js/heart.js
+++ js/heart.js
@@ -1,25 +1,43 @@
-// document.getElementById(unliked).style.display = "none";
+// // document.getElementById(unliked).style.display = "none";
 
-var check;
+// var check;
+
+// function changeImg() {
+//     if (check == null || check == false) {
+//         alert("hi");
+//         document.getElementById(unliked).style.display = "block";
+//         document.getElementById(liked).style.display = "none";
+//         check = true;
+//     } else {
+//         alert("hi");
+//         document.getElementById(unliked).style.display = "none";
+//         document.getElementById(liked).style.display = "block";
+//         check = false;
+//     }
+// }
+
+// if (document.getElementById(unliked).style.display == "block") {
+//     alert("hi");
+// }
+
+// document.getElementById(unliked).style.display == "none";
+
+// document.getElementById(liked).style.display == "block";
+
 
 function changeImg() {
-    if (check == null || check == false) {
-        alert("hi");
-        document.getElementById(unliked).style.display = "block";
-        document.getElementById(liked).style.display = "none";
-        check = true;
-    } else {
-        alert("hi");
-        document.getElementById(unliked).style.display = "none";
-        document.getElementById(liked).style.display = "block";
-        check = false;
-    }
+    // if (document.getElementById("unliked").style.display == "none") {
+    //     alert("hi");
+    //     document.getElementById("unliked").style.display = "block";
+    //     document.getElementById("liked").style.display = "none";
+    // } else {
+    //     document.getElementById("unliked").style.display = "none";
+        // document.getElementById("liked").style.display = "block";
+    // }
+
+    // document.getElementById("unliked").src = "/images/heart.png";
+
 }
 
-if (document.getElementById(unliked).style.display == "block") {
-    alert("hi");
-}
-
-document.getElementById(unliked).style.display == "none";
-
-document.getElementById(liked).style.display == "block";
+// document.getElementById("unliked").src = "/images/heart.png";
+// document.getElementById("unliked").src = "/images/heart.png";
