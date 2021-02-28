f7a61eae5ee86529f185d31bf7a38166c880fbf4
Lily Meng <LilyxMeng@github.com>
2021-02-27 22:39:41 -0500

    commit

--- js/search.js
+++ js/search.js
@@ -1,3 +1,27 @@
-if (text == "weirdtext") { 
-    document.getElementById('boxname').style.display = "none";  
-   }
+var searhValue = document.getElementById("search-bar")
+
+function grabSearch() {
+    if (searhValue.value == "Mandy's Ice Cream") { 
+        /*document.getElementById("mandy").style.display = "grid";*/ //change flex to something else
+        document.getElementById("rocco").style.display = "none";
+        document.getElementById("dine").style.display = "none";
+        console.log("Function Worked!")
+       }
+    else if (searhValue.value == "Rocco Thrift$") { 
+        document.getElementById("mandy").style.display = "none";
+        document.getElementById("dine").style.display = "none";
+        console.log("Function Worked!")
+       }
+    else if (searhValue.value == "Dine & Dip") { 
+        document.getElementById("mandy").style.display = "none";
+        document.getElementById("rocco").style.display = "none";
+        console.log("Function Worked!")
+       }
+    else {
+        document.getElementById("mandy").style.display = "none";
+        document.getElementById("rocco").style.display = "none";
+        document.getElementById("dine").style.display = "none";
+        console.log("Could not get item.")
+    }
+}
+
