b5c69924a3f029f8e1cb0852af9d55dedb802876
Bech <tobechionwuka@gmail.com>
2021-02-28 10:04:37 -0500

    updates to feed

--- js/heart.js
+++ js/heart.js
@@ -1,12 +1,25 @@
 // document.getElementById(unliked).style.display = "none";
 
+var check;
+
 function changeImg() {
-    if (document.getElementById(unliked).src == "none") {
+    if (check == null || check == false) {
+        alert("hi");
         document.getElementById(unliked).style.display = "block";
         document.getElementById(liked).style.display = "none";
+        check = true;
     } else {
         alert("hi");
         document.getElementById(unliked).style.display = "none";
         document.getElementById(liked).style.display = "block";
+        check = false;
     }
 }
+
+if (document.getElementById(unliked).style.display == "block") {
+    alert("hi");
+}
+
+document.getElementById(unliked).style.display == "none";
+
+document.getElementById(liked).style.display == "block";
